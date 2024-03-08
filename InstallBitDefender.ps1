2f3582d2f7afd3e555ad66e6f0a07f73d0e8ceed
Iwklos <79278312+Iwklos@users.noreply.github.com>
2024-03-08 12:50:03 -0600

    Add comments

--- InstallBitDefender.ps1
+++ InstallBitDefender.ps1
@@ -1,46 +1,61 @@
+#Install BitDefender Script
 
+#Change this to org name
+$org = "$env:NINJA_ORGANIZATION_NAME"
+
+#installation path
+$fp = "C:\temp"
+#Get os type
+$OS = (Get-ComputerInfo).OsProductType
+
+#API Key:
 $Key="MDRmZWZiODZlMTk4NTMxNjg3ODMxMTgwM2Q0MGE0OTk2NGFlYmVjMzBlYmEyOTVkZjgwMWYxNjUxOGRkOTBmYjo="
+
+#Request Headers
 $Headers = @{
     Authorization = "Basic $Key";
 }
 
-$OS = (Get-ComputerInfo).OsProductType
-
+#Request Body
 if ($OS = "WorkStation"){
     $Body = '{
         "params": {
       
-          "packageName": "$NINJA_ORGANIZATION_NAME Client"
+          "packageName": "replace_t Client"
       },
         "jsonrpc": "2.0",
         "method": "getInstallationLinks",
         "id": "1"
-     }  '
+     }  ' | foreach {$_.replace('replace_t',$org)}
  }
 else {
     $Body = '{
         "params": {
       
-          "packageName": "$NINJA_ORGANIZATION_NAME Server"
+          "packageName": "replace_t Server"
       },
         "jsonrpc": "2.0",
         "method": "getInstallationLinks",
         "id": "1"
-     }  '
+     }  ' | foreach {$_.replace('replace_t',$org)}
 }
 
+
+
+
+#API Request
 $list = Invoke-RestMethod -Uri "https://cloud.gravityzone.bitdefender.com/api/v1.0/jsonrpc/packages" -Headers $Headers -Method "POST" -ContentType "application/json" -Body $Body
 
+#Format and extract url
 $i = $list.result | Out-String
-
 $a = $i.Split("`n")
-
 foreach($t in $a){
     if ($t.Contains("fullKitWindowsX64")){
         $url = $t.TrimStart("fullKitWindowsX64        : ")
     }
 }
 
-
-Invoke-WebRequest $url -Headers $Headers -OutFile C:\temp
-Start-Process -FilePath "C:\temp\*epskit*.exe" /quiet
+#Get Installation File
+Invoke-WebRequest -Uri $url -Headers $Headers -OutFile $fp\BD.zip
+Expand-Archive "$fp\BD.zip" -DestinationPath C:\temp
+Start-Process -FilePath "$fp\*epskit*.exe" /quiet
