991cc2ce83dad546685e7994dad8d04f864f5590
Iwklos <79278312+Iwklos@users.noreply.github.com>
2024-04-12 08:07:42 -0500

    Edited and Submitted Switch Prompts

--- PowerShell/WGU/Switch_Prompts.ps1
+++ PowerShell/WGU/Switch_Prompts.ps1
@@ -1,28 +1,46 @@
+
 # Isaiah Klosterman, StudentID: 010467788
 
-$sourceFiles = .\Requirements1.zip
 $Date = Get-Date
 
-while ($input != 5) {
-    
-    $input = read-host()
+# Do not end until 5 is selected
+try {
+    while ($i -ne 5) {
+        
+        # Get input selection
+        $i = read-host -Prompt ("Enter Input (1-5)")
 
 
-    switch($input){
-        1{
-            foreach($sourceFile in (Get-ChildItem -filter '*.zip'))
-            {
-                # Read contents of requirements zip file, filter, format and output to daily log file
-                [IO.Compression.ZipFile]::OpenRead($sourceFile.FullName).Entries.FullName -like "*.log" | %{ "$Date $_" } | Add-Content -Path DailyLog.txt
+        switch ($i) {
+            1 {
+                # Iterate through the folder
+                foreach ($var in dir) {
+
+                    # Get log files in requirements file, filter, date and output to daily log file
+                    if ($var.Name -like "*.log") {
+                        $var | % { "$Date $_" } | Add-Content -Path DailyLog.txt
+                    }
+                }
+            }
+            2 {
+                # Get names of entire files in requirements folder and output it to  C916contents file
+                dir | Sort -Property Fullname | Format-Table >> C916contents.txt
+            }
+            3 {
+                # Output CPU and Memory
+                $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+                $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+                'CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB'
+            }
+            4 {
+                # List processes formatted
+                Get-Process | sort vm | select name, vm 
+            
+            }
+            5 { Break }
         }
     }
-        2{}
-        3{
-            $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
-            $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
-            write-output 'CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)'
-        }
-        4{}
-        5{}
-    }
+}
+catch [System.OutOfMemoryException] {
+    "Out of memory"
 }
